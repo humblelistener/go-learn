93d4d2c18d9b8261c557742f819e0a7240ba1b89
Karthik Padmanabhan {PK} <karthi.email@gmail.com>
2015-06-28 20:18:49 +1000

    using pointers - isnt it cool

--- pointers.go
+++ pointers.go
@@ -10,14 +10,14 @@ func main(){
   fmt.Println("my name is", name, ". This info is stored in", &name);
 
   //by default, go passes parameters by value
-  addMyLastName(name)
+  addMyLastName(&name)
 
-  fmt.Println("but still my name is", name)
+  fmt.Println("and now my name is", name)
 }
 
-func addMyLastName(name string) string {
-  name = "kavin padmanabhan"
-  fmt.Println("my name per that method is", name)
+func addMyLastName(name *string) string {
+  *name = "kavin padmanabhan"
+  fmt.Println("my name per that method is", *name)
 
-  return name
+  return *name
 }
