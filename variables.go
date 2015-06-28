36cda0e68a079a5b1ea99c92f15ddeab5af1506e
Karthik Padmanabhan {PK} <karthi.email@gmail.com>
2015-06-28 19:52:32 +1000

    variables declaration

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+var (
+  name, favGame string
+  age int
+)
+
+func main() {
+  name = "kavin"
+  age = 4
+  fmt.Println("My name is", name, "and my age is", age)
+}
